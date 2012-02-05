efb5b478f613ab2351a2ddce21773f5226aa9126
evenwu <evendesign@gmail.com>
2012-02-05 20:51:32 +0800

    fix style and info

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,6 +1,24 @@
+function initLayout(){
+  var window_width = $(window).width();
+  if ( window_width > 960 ) {
+    wrapper_width = Math.floor( window_width / 240 ) * 240;
+    $('.wrapper').css("width", wrapper_width + "px" );
+  }
+}
+
+function resort(){
+  $('#masonry').masonry({
+    itemSelector : '.item',
+    isResizable: false
+  });
+}
 
 $(function(){
-  $('#container').masonry({
-    itemSelector : '.item'
-  });
+  initLayout();
+  resort();
+});
+
+$(window).resize(function(){
+  initLayout();
+  resort();
 });
